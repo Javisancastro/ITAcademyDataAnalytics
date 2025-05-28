c8abbf7fde03458ba5e8909c48e05923ab14f835
Javisancastro <115885157+Javisancastro@users.noreply.github.com>
2025-05-28 13:11:55 +0200

    Add files via upload

---
+++ Sprint 7 Python/Training1.py
@@ -0,0 +1,9 @@
+# Modulo Print - sirve para imprimir en pantalla
+# Autor: Javier Sanchez
+print ("Hello World")
+print ("Hello World 2") 
+print ("Python", "es", "genial")
+print ("Python", "es", "genial", sep="-")
+print ("Python", "es", "genial", sep="-", end="!")
+print ("Python", "es", "genial", sep="-", end="!\n")
+print ("Python", "es", "genial", sep="-", end="!\n\n")
